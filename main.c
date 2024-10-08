55c8291bf2392129d78daaca9e1c15aba4e466b7
Joseph Woodward <joseph.woodward@xeuse.com>
2024-10-09 00:52:08 +0100

    work on terminating connections

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
 #define BUFFER_SIZE 1024
@@ -16,6 +17,7 @@ typedef struct {
 
 typedef struct {
     int client_fd;
+    int closed;
 } tcp_connection;
 
 void *manage_connection(void *args) {
@@ -23,12 +25,12 @@ void *manage_connection(void *args) {
     ssize_t readVal;
     char buffer[BUFFER_SIZE] = {0};
 
-    while ((readVal = read(conn->client_fd, buffer, BUFFER_SIZE))) {
-	printf("[fd %d]: %s", conn->client_fd, buffer);
+    while ((readVal = read(conn->client_fd, buffer, BUFFER_SIZE) != -1)) {
+	printf("[fd %d]: %s\n", conn->client_fd, buffer);
+
 	/* char message[] = "HTTP/1.1 200 OK\r\n\r\n<html>Hello World!</html>\r\n\r\n"; */
 	char message[] = "pong\n";
 	send(conn->client_fd, message, strlen(message), 0);
-	/* break; */
     }
 
     return NULL;
@@ -58,14 +60,15 @@ int start_server(http_server *s) {
 
     printf("Server started and listening on port %d\n", s->port);
 
-    // TODO: track multiple connections
     tcp_connection clients[MAX_CLIENTS];
-    pthread_t thread_id[MAX_CLIENTS];
+    pthread_t thread_id;
 
-    memset(clients, 0, sizeof(clients));
+    // default to -1 so we can check if set
+    memset(clients, -1, sizeof(clients));
 
-    int i;
+    int n;
     while (1) {
+	// waiting for connection
 	int client_fd = accept(server_socket, NULL, NULL);
 	if (client_fd < 0) {
 	    perror("failed to connect to client");
@@ -73,27 +76,43 @@ int start_server(http_server *s) {
 	}
 
 	// TODO: Add timeout when establishing connection
-	printf("connection accepted\n");
 
-	tcp_connection conn = {
-	    .client_fd = client_fd,
-	};
+	// examine existing connections to see if we can establish new ones
+	int i;
+	for (i = 0; i < MAX_CLIENTS; i++) {
+	    tcp_connection conn = {
+		.client_fd = client_fd,
+	    };
 
-	clients[i] = conn;
-
-	if (pthread_create(&thread_id[i], NULL, manage_connection, &clients[i])) {
-	    perror("failed to manage connection");
-	    exit(EXIT_FAILURE);
+	    // if socket is -1 (set by memset above) then we have space available and can accept connection
+	    if (clients[i].client_fd == -1) {
+		printf("connection accepted...\n");
+		clients[i] = conn;
+		break;
+	    }
 	}
 
-	i++;
+	printf("loop index is %d\n", i);
+	if (n <= MAX_CLIENTS) {
+	    // launch thread listening to socket
+	    if (pthread_create(&thread_id, NULL, manage_connection, &clients[i])) {
+		perror("failed to create connection thread");
+		exit(EXIT_FAILURE);
+	    }
+
+	    /* pthread_detach(thread_id); */
+	} else {
+	    printf("max clients of %d reached", MAX_CLIENTS);
+	    close(client_fd);
+	}
+	n++;
     }
 
     return 0;
 }
 
 int main(void) {
-    http_server s = {.port = 8087};
+    http_server s = {.port = 8086};
     start_server(&s);
 
     return 0;
